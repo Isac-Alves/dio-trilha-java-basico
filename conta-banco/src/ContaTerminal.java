78f566f215cbe588552e71337ec4882841d1d474
Isac-Alves <isacalves2011@gmail.com>
2024-08-12 11:54:59 -0300

    feat: Criar o main com a criação de conta

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = input.nextLine();
+
+        System.out.println("Digite a agência: ");
+        String agencia = input.nextLine();
+
+        System.out.println("Digite o número da agência: ");
+        int numero = input.nextInt();
+
+        System.out.println("Informe seu saldo: ");
+        double saldo = input.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta " + numero +" e seu saldo "+ saldo + " já está disponível para saque.");
+        
+        input.close();
     }
 }
